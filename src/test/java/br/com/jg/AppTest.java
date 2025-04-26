79795941c7c0b4ecac670142db706e55465dbb50
Joice Gonçalves <joice.silva.s2@hotmail.com>
2025-04-25 22:51:58 -0300

    fix errors

--- src/test/java/br/com/jg/AppTest.java
+++ src/test/java/br/com/jg/AppTest.java
@@ -1,20 +1,11 @@
 package br.com.jg;
 
+import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testMethod() {
+        assertTrue(true);
     }
 }
